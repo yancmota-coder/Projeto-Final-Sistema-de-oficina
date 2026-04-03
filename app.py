fae5c64229b8070be429147c2fc8dda126ade31a
Guilherme de Sousa <guilhermerodriguess1403@gmail.com>
2026-04-03 16:46:25 -0300

    feat: aula 3 - mecanicos e estoque funcionando

--- app.py
+++ app.py
@@ -58,7 +58,12 @@ def deletar_cliente(id):
     conn = conectar()
     cursor = conn.cursor()
 
+    # 1. Apagar veículos do cliente
+    cursor.execute("DELETE FROM veiculos WHERE cliente_id=%s", (id,))
+
+    # 2. Apagar cliente
     cursor.execute("DELETE FROM clientes WHERE id=%s", (id,))
+
     conn.commit()
 
     return redirect("/clientes")
@@ -114,6 +119,149 @@ def criar_veiculo():
     conn.commit()
 
     return redirect("/clientes")
+
+@app.route("/mecanicos")
+def listar_mecanicos():
+    conn = conectar()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM mecanicos")
+    mecanicos = cursor.fetchall()
+
+    return render_template("mecanicos.html", mecanicos=mecanicos)
+
+@app.route("/mecanicos/novo")
+def novo_mecanico():
+    return render_template("novo_mecanico.html")
+
+@app.route("/mecanicos/criar", methods=["POST"])
+def criar_mecanico():
+    nome = request.form["nome"]
+    especialidade = request.form["especialidade"]
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO mecanicos (nome, especialidade) VALUES (%s, %s)",
+        (nome, especialidade)
+    )
+    conn.commit()
+
+    return redirect("/mecanicos")
+
+@app.route("/mecanicos/editar/<int:id>")
+def editar_mecanico(id):
+    conn = conectar()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM mecanicos WHERE id=%s", (id,))
+    mecanico = cursor.fetchone()
+
+    return render_template("editar_mecanico.html", mecanico=mecanico)
+
+@app.route("/mecanicos/deletar/<int:id>")
+def deletar_mecanico(id):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    # 2. Apagar mecanico
+    cursor.execute("DELETE FROM mecanicos WHERE id=%s", (id,))
+
+    conn.commit()
+
+    return redirect("/mecanicos")
+
+@app.route("/mecanicos/atualizar/<int:id>", methods=["POST"])
+def atualizar_mecanico(id):
+    nome = request.form["nome"]
+    especialidade = request.form["especialidade"]
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE mecanicos SET nome=%s, especialidade=%s WHERE id=%s",
+        (nome, especialidade, id)
+    )
+
+    conn.commit()
+
+    return redirect("/mecanicos")
+
+@app.route("/pecas")
+def listar_pecas():
+    conn = conectar()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM pecas")
+    pecas = cursor.fetchall()
+
+    return render_template("pecas.html", pecas=pecas)
+
+@app.route("/pecas/novo")
+def nova_peca():
+    return render_template("nova_peca.html")
+
+@app.route("/pecas/criar", methods=["POST"])
+def criar_peca():
+    nome = request.form["nome"]
+    codigo = request.form["codigo"]
+    quantidade = int(request.form["quantidade_estoque"])
+    preco = float(request.form["preco_venda"])
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO pecas (nome, codigo, quantidade_estoque, preco_venda) VALUES (%s, %s, %s, %s)",
+        (nome, codigo, quantidade, preco)
+    )
+
+    conn.commit()
+
+    return redirect("/pecas")
+
+@app.route("/pecas/editar/<int:id>")
+def editar_peca(id):
+    conn = conectar()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM pecas WHERE id=%s", (id,))
+    peca = cursor.fetchone()
+
+    return render_template("editar_peca.html", peca=peca)
+
+@app.route("/pecas/atualizar/<int:id>", methods=["POST"])
+def atualizar_peca(id):
+    nome = request.form["nome"]
+    codigo = request.form["codigo"]
+    quantidade_estoque = int(request.form["quantidade_estoque"])
+    preco_venda = float(request.form["preco_venda"])
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE pecas SET nome=%s, codigo=%s, quantidade_estoque=%s, preco_venda=%s WHERE id=%s",
+        (nome, codigo, quantidade_estoque, preco_venda, id)
+    )
+
+    conn.commit()
+
+    return redirect("/pecas")
+
+@app.route("/pecas/deletar/<int:id>")
+def deletar_peca(id):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    # 2. Apagar peça
+    cursor.execute("DELETE FROM pecas WHERE id=%s", (id,))
+
+    conn.commit()
+
+    return redirect("/pecas")
 
 if __name__ == "__main__":
     app.run(debug=True)
